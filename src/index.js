e47db2f6a372a5d040b160c4b11a71b1fad76ef4
TonyFef <fefilov.toha1999@yandex.ru>
2022-03-18 19:19:41 +0300

    add documents shower

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { headerModal } from "./modules/headerModal";
 import { ruler } from "./modules/ruler";
 import { timer } from "./modules/timer";
+import { documents } from "./modules/documents";
 
 const headerModalWindow = document.getElementById("header-modal");
 const headerModalButton = document.getElementById("headerModalButton");
 const headerModalClose = document.getElementById("headerModalClose");
 const overlay = document.querySelector(".overlay");
 
-// const servicesModalClose = document.getElementById("headerModalClose");
-
 headerModal(headerModalWindow, headerModalButton, headerModalClose, overlay);
 ruler(overlay);
-setTimeout(timer, 1, "26 march 2022")
+setTimeout(timer, 1, "26 march 2022");
+documents(overlay);
---
+++ src/modules/documents.js
@@ -0,0 +1,62 @@
+import { animate } from "./helpers";
+
+export const documents = (secondOverlay) => {
+    const docsImg = document.querySelectorAll(".sertificate-document");
+    // const docsOverlays = document.querySelectorAll(".document-overlay");
+    console.log(docsImg);
+
+    docsImg.forEach((doc) => {
+        doc.addEventListener("mouseenter", (e) => {
+            const docLink = e.target.closest("a");
+            const overlay = docLink.querySelector(".document-overlay");
+            // overlay.style.opacity = 1;
+            animate({
+                duration: 150,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    overlay.style.opacity = progress;
+                },
+            });
+        });
+        doc.addEventListener("mouseleave", (e) => {
+            const docLink = e.target.closest("a");
+            const overlay = docLink.querySelector(".document-overlay");
+
+            animate({
+                duration: 150,
+                timing(timeFraction) {
+                    return timeFraction;
+                },
+                draw(progress) {
+                    overlay.style.opacity = progress / progress - progress;
+                },
+            });
+        });
+        doc.addEventListener("click", (e) => {
+            console.log(secondOverlay);
+            const docModal = document.querySelector("#bigDoc");
+            docModal.classList.add("show");
+            docModal.classList.remove("none");
+            secondOverlay.style.display = "block";
+            e.preventDefault();
+
+            secondOverlay.addEventListener("click", () => {
+                docModal.classList.add("none");
+                secondOverlay.style.display = "none";
+            });
+        });
+    });
+};
+
+const shower = () => {
+    const docsSection = document.getElementById("documents");
+    const bigDoc = document.createElement("img");
+    bigDoc.src = "images/documents/document4.jpg";
+    bigDoc.id = "bigDoc";
+    bigDoc.classList.add("none");
+    // bigDoc.classList.add("show");
+    docsSection.append(bigDoc);
+};
+shower();
