62eab7a6b8f8c3f51439588d36a83f0e36ad79b7
TonyFef <fefilov.toha1999@yandex.ru>
2022-03-18 15:05:24 +0300

    add timer

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -3,4 +3,40 @@ export const timer = (deadline) => {
     const timerHours = document.getElementById("timer-hours");
     const timerMinutes = document.getElementById("timer-minutes");
     const timerSeconds = document.getElementById("timer-seconds");
+
+    const getTimeRemaining = () => {
+        let dateStop = new Date(deadline).getTime();
+        let dateNow = new Date().getTime();
+        let timeRemaining = (dateStop - dateNow) / 1000;
+        let days = Math.floor(timeRemaining / 60 / 60 / 24);
+        let hours = Math.floor((timeRemaining / 60 / 60) % 24);
+        let minutes = Math.floor((timeRemaining / 60) % 60);
+        let seconds = Math.floor(timeRemaining % 60);
+
+        return { timeRemaining, days, hours, minutes, seconds };
+    };
+    const updateClock = () => {
+        let getTime = getTimeRemaining();
+
+        if (getTime.hours < 10) {
+            timerHours.textContent = `0${getTime.hours}`;
+        } else {
+            timerHours.textContent = getTime.hours;
+        }
+        let minutesShower = getTime.minutes < 10 ? (timerMinutes.textContent = `0${getTime.minutes}`) : (timerMinutes.textContent = getTime.minutes);
+        let secondsShower = getTime.seconds < 10 ? (timerSeconds.textContent = `0${getTime.seconds}`) : (timerSeconds.textContent = getTime.seconds);
+        let daysShower = getTime.days < 10 ? (timerDays.textContent = `0${getTime.days}`) : (timerDays.textContent = getTime.days);
+
+        if (getTime.timeRemaining < 0) {
+            timerDays.textContent = "00";
+            timerHours.textContent = "00";
+            timerMinutes.textContent = "00";
+            timerSeconds.textContent = "00";
+        }
+
+        if (getTime.timeRemaining > 0) {
+            setTimeout(updateClock, 1000);
+        }
+    };
+    updateClock();
 };
