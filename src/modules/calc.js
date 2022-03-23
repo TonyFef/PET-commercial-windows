2842ac82bb3132f3b682deb7b1c8c6638e5bb605
TonyFef <fefilov.toha1999@yandex.ru>
2022-03-23 16:08:45 +0300

    fixed calc

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -2,56 +2,60 @@ import { animate } from "./helpers";
 
 export const calc = () => {
     const calcBlock = document.getElementById("calc");
-    const calcType = document.querySelector("#calc-type");
-    const calcGlass = document.querySelector("#calc-type-material");
-    const calcSquare = document.querySelector("#calc-input");
-    const total = document.getElementById("calc-total");
+    if (calcBlock) {
+        const calcType = document.querySelector("#calc-type");
+        const calcGlass = document.querySelector("#calc-type-material");
+        const calcSquare = document.querySelector("#calc-input");
+        const total = document.getElementById("calc-total");
 
-    const countCalc = () => {
-        let totalValue = 0;
+        const countCalc = () => {
+            let totalValue = 0;
 
-        const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
-        let calcGlassValue = +calcGlass.value;
-        let calcSquareValue = +calcSquare.value;
+            const calcTypeValue = +calcType.options[calcType.selectedIndex].value;
+            let calcGlassValue = +calcGlass.value;
+            let calcSquareValue = +calcSquare.value;
 
-        calcSquare.addEventListener("input", (e) => {
-            e.target.value = e.target.value.replace(/[\D+\-]/g, "");
+            calcSquare.addEventListener("input", (e) => {
+                e.target.value = e.target.value.replace(/[\D+\-]/g, "");
+            });
+
+            if (calcSquare.value && calcType.value) {
+                totalValue = calcSquareValue * calcTypeValue;
+
+                animate({
+                    duration: 250,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        total.value = (+progress * totalValue).toFixed();
+                    },
+                });
+            }
+            if (calcSquare.value && calcType.value && calcGlassValue) {
+                totalValue = calcGlassValue * calcSquareValue * calcTypeValue;
+
+                animate({
+                    duration: 250,
+                    timing(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw(progress) {
+                        total.value = (+progress * totalValue).toFixed();
+                    },
+                });
+            }
+            if (calcSquareValue == "") {
+                total.value = 0;
+            }
+        };
+
+        calcBlock.addEventListener("input", (e) => {
+            if (e.target === calcType || e.target === calcGlass || e.target === calcSquare) {
+                countCalc();
+            }
         });
+    }
 
-        if (calcSquare.value && calcType.value) {
-            totalValue = calcSquareValue * calcTypeValue;
-
-            animate({
-                duration: 250,
-                timing(timeFraction) {
-                    return timeFraction;
-                },
-                draw(progress) {
-                    total.value = (+progress * totalValue).toFixed();
-                },
-            });
-        }
-        if (calcSquare.value && calcType.value && calcGlassValue) {
-            totalValue = calcGlassValue * calcSquareValue * calcTypeValue;
-
-            animate({
-                duration: 250,
-                timing(timeFraction) {
-                    return timeFraction;
-                },
-                draw(progress) {
-                    total.value = (+progress * totalValue).toFixed();
-                },
-            });
-        }
-        if (calcSquareValue == "") {
-            total.value = 0;
-        }
-    };
-
-    calcBlock.addEventListener("input", (e) => {
-        if (e.target === calcType || e.target === calcGlass || e.target === calcSquare) {
-            countCalc();
-        }
-    });
+    // console.log(calcBlock);
 };
