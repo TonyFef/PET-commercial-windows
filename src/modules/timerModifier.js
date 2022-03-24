ebea9d9c781b7acb7c232d61bb4a3f55ee1a1478
TonyFef <fefilov.toha1999@yandex.ru>
2022-03-24 23:03:33 +0300

    fixed timerModifier

---
+++ src/modules/timerModifier.js
@@ -0,0 +1,16 @@
+export const declOfNum = (n, textForms) => {
+    n = Math.abs(n) % 100;
+    // console.log(n);
+    let n1 = n % 10;
+    // console.log(n1);
+    if (n > 10 && n < 20) {
+        return textForms[2];
+    }
+    if (n1 > 1 && n1 < 5) {
+        return textForms[1];
+    }
+    if (n1 == 1) {
+        return textForms[0];
+    }
+    return textForms[2];
+};
